8d1a227225d27fba078a8ea7c4701e5494d58607
Syedfarz7860 <syedfarazdakmehdi110@gmail.com>
2026-09-26 11:29:36 +0530

    Connect frontend to deployed backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://ecommerce-backend-e7ql.onrender.com/api",  // ✔ correct
-   baseURL: "http://localhost:5001/api"
+  baseURL: "https://ecommerce-backend-e7ql.onrender.com/api",
 });
-
 
 export default api;
